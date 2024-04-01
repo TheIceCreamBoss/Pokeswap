1da619aa3614bad8b5c920824cfe7d25237fa9bb
Jay Park <jaybinpark@gmail.com>
2024-04-01 15:40:53 -0700

    setup html/css/js frontend

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 var app = express();
 app.use(express.json());
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dbRouter = require('./routes/database');
 var tradeRouter = require('./routes/trade');
@@ -18,17 +18,16 @@ var commentsRouter = require('./routes/comments');
 var mysql = require('mysql2');
 const port = 3001
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/db', dbRouter);
+app.use('/', dbRouter);
 app.use('/trade', tradeRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
 
 // connection.end()
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -45,6 +44,10 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -59,7 +62,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page 
   res.status(err.status || 500);
-  res.render('index', { title:  res.locals.error});
+  res.send( { "title" :  res.locals.error});
 });
 
 app.listen(port, () => {
