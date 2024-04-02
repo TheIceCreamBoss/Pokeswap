9342e190afea67d62a307456f935918d84f0e02e
TheIceCreamBoss <nicholaszhang0817@gmail.com>
2024-04-01 22:26:43 -0700

    Lets GOOOOOOOOOO

---
+++ app/service/cardTypesService.js
@@ -0,0 +1,115 @@
+var mysql = require('mysql2');
+
+var connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS
+});
+
+async function getEnergyCards() {
+    console.log('getEnergyCards'); 
+    return new Promise((resolve, reject) => {
+
+        connection.query('USE pokeswap');
+
+        connection.query('SELECT * FROM energyCard', function (err, results) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(results);
+                resolve(results);
+            }
+        });
+    });
+}
+
+async function getEnergyCardDescriptions() {
+    console.log('getEnergyCardDescriptions'); 
+    return new Promise((resolve, reject) => {
+
+        connection.query('USE pokeswap');
+
+        connection.query('SELECT * FROM energyCardDescriptions', function (err, results) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(results);
+                resolve(results);
+            }
+        });
+    });
+}
+async function getPokemonCards() {
+    console.log('getPokemonCards'); 
+    return new Promise((resolve, reject) => {
+
+        connection.query('USE pokeswap');
+
+        connection.query('SELECT * FROM pokemonCard', function (err, results) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(results);
+                resolve(results);
+            }
+        });
+    });
+}
+async function getPokemonCardsTypes() {
+    console.log('getPokemonCardsTypes'); 
+    return new Promise((resolve, reject) => {
+
+        connection.query('USE pokeswap');
+
+        connection.query('SELECT * FROM pokemonTypes', function (err, results) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(results);
+                resolve(results);
+            }
+        });
+    });
+}
+async function getTrainerCards() {
+    console.log('getTrainerCards'); 
+    return new Promise((resolve, reject) => {
+
+        connection.query('USE pokeswap');
+
+        connection.query('SELECT * FROM trainerCard', function (err, results) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(results);
+                resolve(results);
+            }
+        });
+    });
+}
+
+async function getTrainerCardDescriptions() {
+    console.log('getTrainerCardDescriptions'); 
+    return new Promise((resolve, reject) => {
+
+        connection.query('USE pokeswap');
+
+        connection.query('SELECT * FROM trainerCardDescriptions', function (err, results) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(results);
+                resolve(results);
+            }
+        });
+    });
+}
+
+module.exports = {
+    getEnergyCards,
+    getEnergyCardDescriptions,
+    getPokemonCards,
+    getPokemonCardsTypes,
+    getTrainerCardDescriptions,
+    getTrainerCards
+}
